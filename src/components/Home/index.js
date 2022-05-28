a1b2abb686e9bd7bbd5bb12cc030f6d69f51cfa4
Jacob Jun <jacobsunjun@gmail.com>
2022-05-27 18:26:51 -0700

    Add blobs to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import ContactButton from './ContactButton'
 // @ts-ignore
 import SelfPortrait from '../../assets/images/Self-portrait.png'
 import './index.scss'
@@ -65,6 +64,12 @@ const Home = () => {
             CONTACT ME
           </Link>
         </div>
+        <svg id="blob1" className="blob" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#005B95" d="M70.4,-16.6C79.7,5.8,67.8,41.4,46.5,54.7C25.2,68,-5.6,59,-26.9,42.3C-48.3,25.5,-60.2,1,-54.2,-16.9C-48.1,-34.8,-24.1,-46.1,3.2,-47.2C30.5,-48.2,61.1,-39,70.4,-16.6Z" transform="translate(100 100)" />
+        </svg>
+        <svg className="blob" id="blob2" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#393E46" d="M58.7,-17.7C64.8,0,51,25.6,30.9,39.5C10.8,53.3,-15.6,55.5,-33.5,43.2C-51.4,30.9,-60.8,4,-53.8,-14.7C-46.8,-33.5,-23.4,-44.1,1.4,-44.6C26.2,-45.1,52.5,-35.3,58.7,-17.7Z" transform="translate(100 100)" />
+        </svg>
         <img className="self-portrait" src={SelfPortrait} alt="Profile picture" />
       </div>
       <Loader type="pacman" />
